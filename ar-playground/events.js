afcc1b5638b659e483fe46df4db64840ee0db3d4
nicolo.carpignoli <nicolo.carpignoli@chialab.it>
2019-03-19 23:53:24 +0100

    chore: trying to fix click events

--- ar-playground/events.js
+++ ar-playground/events.js
@@ -3,9 +3,9 @@ AFRAME.registerComponent('emitter', {
     init: function() {
         console.log('this.el', this.el)
         // every click, we make our model grow in size :)
-        this.addEventListener('click', function(ev){
+        this.el.addEventListener('click', function(ev){
             const cursorEl = ev && ev.detail && ev.detail.cursorEl;
-            console.log(cursorEl)
+            console.log(cursorEl, this)
             if (cursorEl === this) {
                 // const scale = aEntity.getAttribute('scale');
                 // Object.keys(scale).forEach((key) => scale[key] = scale[key] + 1);
