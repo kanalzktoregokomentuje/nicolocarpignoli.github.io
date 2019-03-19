4ca831d694f0b4c5fcab53a9c580d9f99888b02e
nicolo.carpignoli <nicolo.carpignoli@chialab.it>
2019-03-19 22:45:26 +0100

    chore: trying to fix click events

--- ar-playground/events.js
+++ ar-playground/events.js
@@ -5,12 +5,11 @@ AFRAME.registerComponent('markerhandler', {
         const aEntity = document.querySelector("#animated-model");
 
         // every click, we make our model grow in size :)
-        animatedMarker.addEventListener('click', function(ev, target){
-            console.log('event', ev)
-            console.log('target', target
-            )
-            const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
-            if (aEntity && intersectedElement === aEntity) {
+        animatedMarker.addEventListener('click', function(ev){
+            console.log('event', ev.detail)
+
+            const cursor = ev && ev.detail && ev.detail.cursorEl;
+            if (cursor) {
                 // const scale = aEntity.getAttribute('scale');
                 // Object.keys(scale).forEach((key) => scale[key] = scale[key] + 1);
                 // aEntity.setAttribute('scale', scale);
