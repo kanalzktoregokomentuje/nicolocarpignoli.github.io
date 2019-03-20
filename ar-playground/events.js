a3bff554ac65ca011998e9712f856facf82070f4
nicolo.carpignoli <nicolo.carpignoli@chialab.it>
2019-03-20 12:52:11 +0100

    chore trying click on a scene

--- ar-playground/events.js
+++ ar-playground/events.js
@@ -3,6 +3,7 @@ AFRAME.registerComponent('click-listener', {
         const aEntity = document.querySelector('#animated-model');
         this.el.addEventListener('click', function(ev){
             const clickedObject = ev.detail.intersection.object;
+            console.log(clickedObject)
             if (clickedObject && aEntity === clickedObject.parent.el) {
                 console.log('ho cliccato la figura')
             }
