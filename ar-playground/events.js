c152f21d4f1da9e56d4b91fe1e6ade471455912a
nicolo.carpignoli <nicolo.carpignoli@chialab.it>
2019-03-20 11:34:57 +0100

    trying with a-entity cursor

--- ar-playground/events.js
+++ ar-playground/events.js
@@ -1,6 +1,7 @@
 AFRAME.registerComponent('click-listener', {
     init: function() {
         this.el.addEventListener('click', function(ev){
+            console.log(ev.detail)
             if (ev.detail.intersectedEl) {
                 console.log('click a frame', ev.detail.intersectedEl)
             }
