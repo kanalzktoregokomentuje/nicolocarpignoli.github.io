95aec1075e546363f5c50d9c25260e8ae765fa40
nicolo.carpignoli <nicolo.carpignoli@chialab.it>
2019-03-20 10:58:34 +0100

    trying with a-entity cursor

--- ar-playground/events.js
+++ ar-playground/events.js
@@ -1,11 +1,14 @@
-AFRAME.registerComponent('cursor-listener', {
-
+AFRAME.registerComponent('click-listener', {
     init: function() {
         // every click, we make our model grow in size :)
         this.el.addEventListener('click', function(ev){
             // const scale = aEntity.getAttribute('scale');
             // Object.keys(scale).forEach((key) => scale[key] = scale[key] + 1);
             // aEntity.setAttribute('scale', scale);
-            console.log('click', ev.detail)
+            console.log('click a frame', ev.detail)
         });
 }});
+
+document.querySelector('#animated-model').addEventListener((ev) => {
+    console.log('click classico', ev)
+});
