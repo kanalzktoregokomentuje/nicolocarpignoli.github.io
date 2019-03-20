de50d393aae0f5bc7de3df833b1e3ba1caaf6e84
nicolo.carpignoli <nicolo.carpignoli@chialab.it>
2019-03-20 11:49:00 +0100

    chore trying click on a scene

--- ar-playground/events.js
+++ ar-playground/events.js
@@ -1,7 +1,7 @@
 AFRAME.registerComponent('click-listener', {
     init: function() {
         this.el.addEventListener('click', function(ev){
-            console.log(ev.detail)
+            console.log(ev)
             console.log(document.querySelector('#animated-marker'))
             console.log(document.querySelector('#animated-model'))
         });
