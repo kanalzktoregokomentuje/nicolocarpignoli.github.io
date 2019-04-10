aba8d7fc3112b0713c92a404a56b4a2daf02acf4
nicolocarpignoli <nicolocarpignoli@gmail.com>
2019-04-10 21:27:04 +0200

    trying better event click handling

--- ar-experimental/events.js
+++ ar-experimental/events.js
@@ -5,14 +5,5 @@ AFRAME.registerComponent('cursor-listener', {
             console.log('deatil', ev.detail.intersection);
             console.log('cursorEl', ev.detail.cursorEl);
             console.log('targetEl', ev.detail.targetEl);
-
-            // if (animatedMarker.object3D.visible == true && ev.detail.cursorEl) {
-            //     // const entity = document.querySelector('#animated-model');
-            //     // const scale = entity.getAttribute('scale');
-            //     // Object.keys(scale).forEach((key) => scale[key] = scale[key] + 1);
-
-            //     // // every click, we make our model grow in size
-            //     entity.setAttribute('scale', scale);
-            // }
         });
 }});
