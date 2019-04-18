647cae40e2a76e6bdf05ea19166e311fcff70cf2
nicolocarpignoli <nicolocarpignoli@gmail.com>
2019-04-18 22:41:12 +0200

    chore: trying new proj

--- ar-experimental/events.js
+++ ar-experimental/events.js
@@ -1,9 +1,9 @@
-// register our component
-AFRAME.registerComponent('cursor-listener', {
-    init: function() {
-        this.el.addEventListener('click', function(ev){
-            console.log('deatil', ev.detail.intersection);
-            console.log('cursorEl', ev.detail.cursorEl);
-            console.log('targetEl', ev.detail.targetEl);
-        });
-}});
+// // register our component
+// AFRAME.registerComponent('cursor-listener', {
+//     init: function() {
+//         this.el.addEventListener('click', function(ev){
+//             console.log('deatil', ev.detail.intersection);
+//             console.log('cursorEl', ev.detail.cursorEl);
+//             console.log('targetEl', ev.detail.targetEl);
+//         });
+// }});
