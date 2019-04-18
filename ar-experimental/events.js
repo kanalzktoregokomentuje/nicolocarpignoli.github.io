f86fcb09689f2130fa8b9cd60292c06b02524dee
nicolocarpignoli <nicolocarpignoli@gmail.com>
2019-04-18 23:02:48 +0200

    chore trying

--- ar-experimental/events.js
+++ ar-experimental/events.js
@@ -1,9 +1,5 @@
-// // register our component
-// AFRAME.registerComponent('cursor-listener', {
-//     init: function() {
-//         this.el.addEventListener('click', function(ev){
-//             console.log('deatil', ev.detail.intersection);
-//             console.log('cursorEl', ev.detail.cursorEl);
-//             console.log('targetEl', ev.detail.targetEl);
-//         });
-// }});
+window.onload(() => {
+    document.querySelector('#picture').addEventListener('click', () => {
+        alert('sticchi')
+    });
+});
