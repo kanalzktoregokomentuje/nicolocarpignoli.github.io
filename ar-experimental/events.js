e3c7d9b6515182d848b4506a96652534958f9b5b
nicolocarpignoli <nicolocarpignoli@gmail.com>
2019-05-06 18:31:11 +0200

    chore: adding pinch to zoom

--- ar-experimental/events.js
+++ ar-experimental/events.js
@@ -1,5 +1,25 @@
 window.onload(() => {
-    document.querySelector('#picture').addEventListener('click', () => {
-        alert('sticchi')
-    });
+    const camera = document.querySelector('#camera');
+    document.querySelector('body').addEventListener('gestureend', (event) => {
+        let scale = 0;
+        if (event.scale < 1.0) {
+            // User moved fingers closer together
+            scale++;
+        } else if (event.scale > 1.0) {
+            // User moved fingers further apart
+            scale--;
+        }
+        return setZoom(camera, scale);
+    }, false);
 });
+
+const getCurrentZoom = (element) => {
+    let zoom = element.getAttribute('camera');
+    zoom = zoom.split('zoom: ') && zoom.split('zoom: ')[1];
+    return zoom;
+};
+
+const setCurrentZoom = (element, scale) => {
+    const current = getCurrentZoom(element);
+    return element.setAttribute('camera', `camera: ${current + scale};`);
+};
