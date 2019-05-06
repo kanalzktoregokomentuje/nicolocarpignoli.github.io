101ff8247b18bd37e2817e1952f326e34aa6a0cb
nicolocarpignoli <nicolocarpignoli@gmail.com>
2019-05-06 18:34:58 +0200

    chore: fixed bug

--- ar-experimental/events.js
+++ ar-experimental/events.js
@@ -1,17 +1,19 @@
-window.onload(() => {
-    const camera = document.querySelector('#camera');
-    document.querySelector('body').addEventListener('gestureend', (event) => {
-        let scale = 0;
-        if (event.scale < 1.0) {
-            // User moved fingers closer together
-            scale++;
-        } else if (event.scale > 1.0) {
-            // User moved fingers further apart
-            scale--;
-        }
-        return setZoom(camera, scale);
-    }, false);
-});
+AFRAME.registerComponent('markerhandler', {
+    init: function() {
+        const camera = document.querySelector('#camera');
+        console.log('Got camera', camera);
+        document.querySelector('body').addEventListener('gestureend', (event) => {
+            let scale = 0;
+            if (event.scale < 1.0) {
+                // User moved fingers closer together
+                scale++;
+            } else if (event.scale > 1.0) {
+                // User moved fingers further apart
+                scale--;
+            }
+            return setZoom(camera, scale);
+        }, false);
+}});
 
 const getCurrentZoom = (element) => {
     let zoom = element.getAttribute('camera');
